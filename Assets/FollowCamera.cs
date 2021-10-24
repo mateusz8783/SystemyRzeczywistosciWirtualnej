0755f911a8c4fd38e65afc4fd742e62ad67f3c19
mateusz8783 <kasprzyckimateusz@gmail.com>
2021-10-24 23:53:22 +0200

    Initial commit

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+	
+	public Transform lookAtTarget;
+	
+	void FixedUpdate()
+	{
+		transform.LookAt(lookAtTarget.position);
+	}
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+	public Rigidbody rb;
+	public float speed=300;
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+			float horizontal = Input.GetAxis("Horizontal");
+			float vertical = Input.GetAxis("Vertical");
+			
+			Vector3 direction = new Vector3(horizontal,0,vertical);
+			
+			rb.AddForce(direction * speed * Time.deltaTime);
+	}
+}
