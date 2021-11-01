6d6b91c6d0b18e56745a2fe1a660eb38537f40c7
mateusz8783 <kasprzyckimateusz@gmail.com>
2021-11-02 00:28:47 +0100

    Camera Interpolation

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -5,7 +5,17 @@ public class FollowCamera : MonoBehaviour
 {
 	public Transform target;
 	public Vector3 offsetPosition;
+	public Vector3 lookAtOffset;
+	private Vector3 destinationPosition;
+	private Vector3 currentPosition;
+	public float smoothSpeed;
 	public bool lookAt = true;
+
+	private void Start()
+	{
+		transform.position = target. TransformPoint(offsetPosition);
+		currentPosition = offsetPosition;
+	}
 	private void Update()
 	{
 		if(target == null)
@@ -13,7 +23,15 @@ public class FollowCamera : MonoBehaviour
 			Debug. LogWarning("Missing target ref !", this);
 			return;
 		}
-		transform.position = target. TransformPoint(offsetPosition);
+		if(smoothSpeed < 0)
+		{
+			Debug. LogWarning("smoothSpeed < 0 !", this);
+			return;
+		}
+
+		destinationPosition = offsetPosition + lookAtOffset;
+		currentPosition = Vector3.Lerp(currentPosition, destinationPosition, smoothSpeed);
+		transform.position = target. TransformPoint(currentPosition);
 
 		// compute rotation
 		if(lookAt)
